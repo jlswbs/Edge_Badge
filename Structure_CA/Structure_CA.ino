19f7c48495534a5b69505d84c386ff128511ac03
jlswbs <jlswbs@users.noreply.github.com>
2021-06-20 16:47:31 +0200

    Add files via upload

---
+++ Structure_CA/Structure_CA.ino
@@ -0,0 +1,95 @@
+// Structure cellular automata //
+
+#include "Adafruit_Arcada.h"
+
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+
+Adafruit_Arcada arcada;
+
+uint16_t *framebuffer;
+
+#define WIDTH  160
+#define HEIGHT 128
+#define SCR (WIDTH * HEIGHT)
+
+#define ITER 40
+
+  uint8_t state[WIDTH];
+  uint8_t k;
+
+
+void rndrule() {
+
+  memset(framebuffer, 0, 2*SCR);
+
+  for (int x=0; x<WIDTH; x++) state[x] = trngGetRandomNumber()%16;
+  k = trngGetRandomNumber()%16;
+
+}
+
+volatile uint32_t rNum;
+  
+uint32_t trngGetRandomNumber(void){
+  uint32_t i = rNum;
+  TRNG->INTENSET.reg = TRNG_INTENSET_DATARDY;
+  while (i == rNum);
+  TRNG->INTENCLR.reg = TRNG_INTENCLR_DATARDY;
+  return rNum;
+}
+
+void TRNG_Handler(){
+  rNum = TRNG->DATA.reg;
+  TRNG->INTFLAG.bit.DATARDY = 1;
+}
+
+
+void setup(void) {
+
+  MCLK->APBCMASK.reg |= MCLK_APBCMASK_TRNG;
+  NVIC_SetPriority(TRNG_IRQn, 0);
+  NVIC_EnableIRQ(TRNG_IRQn);
+  TRNG->INTENCLR.reg = TRNG_INTENCLR_DATARDY;
+  TRNG->CTRLA.reg = TRNG_CTRLA_ENABLE;
+
+  if (!arcada.arcadaBegin()) {while (1);}
+  arcada.displayBegin();
+  
+  arcada.setBacklight(255);
+
+  if (! arcada.createFrameBuffer(ARCADA_TFT_WIDTH, ARCADA_TFT_HEIGHT)) {while (1);}
+  framebuffer = arcada.getFrameBuffer();
+
+  rndrule();
+ 
+}
+
+void loop() {
+
+  if (arcada.readButtons() & ARCADA_BUTTONMASK_A) rndrule();
+
+  for (int i = 0; i < ITER; i++) {
+  
+  for (int y = 0; y < HEIGHT; y++) {
+  
+    for (int x = 0; x < WIDTH; x++) {
+      
+      k = k ^ state[x] ^ state[k];
+      state[x] = k;
+      framebuffer[x+ARCADA_TFT_WIDTH*y] = (WHITE<<k)^k;
+      
+    }
+    
+  }
+
+  }
+
+  arcada.blitFrameBuffer(0, 0, true, false);
+ 
+}
